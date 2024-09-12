e352bf4c66336119e3c1028133c9d31beb99aaa1
brason-orrey <borrey54@gmail.com>
2024-09-12 13:29:04 -0500

    Update program_2.py
    Brason Orrey - Submission

--- program_2.py
+++ program_2.py
@@ -1,13 +1,19 @@
+from statistics import mean
 
-def average_age():
-    # Get User Input
+Friend1 = input("Please enter one of your friends names:")
+Friend2 = input("Please enter another friends name:")
+Friend3 = input("Please enter a different friends name:")
+Friend4 = input("Please enter a fourth friends name:")
+Friend5 = input("Please enter one more friends name:")
 
+Friend1_age = int(input(f"Please enter {Friend1}'s age:"))
+Friend2_age = int(input(f"Please enter {Friend2}'s age:"))
+Friend3_age = int(input(f"Please enter {Friend3}'s age:"))
+Friend4_age = int(input(f"Please enter {Friend4}'s age:"))
+Friend5_age = int(input(f"Please enter {Friend5}'s age:"))
 
-    # Sum ages
+List_Of_Age__For_Friends = (Friend1_age, Friend2_age, Friend3_age, Friend4_age, Friend5_age)
 
-    # Average the ages
+age_average = mean(List_Of_Age__For_Friends)
 
-    # Print the results
-
-# Line which calls the above function.
-average_age()
+print("The average age of your friends is:", age_average)
