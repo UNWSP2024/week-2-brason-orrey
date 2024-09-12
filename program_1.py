33821427185ecfc1c8940cc67558e85035254c4a
brason-orrey <borrey54@gmail.com>
2024-09-12 10:27:34 -0500

    Update program_1.py
    Brason Orrey Submission

--- program_1.py
+++ program_1.py
@@ -1,6 +1,8 @@
 def personal_information():
-    print('...')
-
+    print ("Brason Orrey")
+    print ("11764 Naples Cir. NE Blaine, MN 55449")
+    print (6127042240)
+    print ("Cybersecuirity")
 
 # Line which calls the above function.
 personal_information()
